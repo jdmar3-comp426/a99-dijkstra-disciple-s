a167f252539f27b6da5c43a95091cc8864afeb51
ayredkklc <ayredkklc@gmail.com>
2021-12-02 19:43:45 -0500

    register working some minor problems with login

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const cors = require("cors")
 const multer = require("multer")
 const upload = multer()
 
+const bodyParser = require('body-parser'); // Middleware 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,6 +36,43 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
+/*app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+  });
+  
+// Route to Login Page
+app.get('/login', (req, res) => {
+	res.sendFile(__dirname + '/login.html');
+  });
+*/
+//for loggin in 
+app.post('/logout', (req, res) => {
+	// Insert Login Code Here
+	request.session.loggedin = false;
+	request.session.username = null;
+	response.send('Logged out succesfully');
+});
+
+app.post('/app/login', function(request, response) {
+	var email = request.body.email;
+	var pass = request.body.pass;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE eamil = ? AND pass = ?', [email, pass], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
     res.json({ "message": "Your API works! (200)" });
@@ -42,17 +85,18 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/user", upload.none(), (req, res, next) => {
     var data = {
-        user: req.body.user,
         email: req.body.email,
         pass: req.body.pass ? md5(req.body.pass) : null
     }
     
-    const stmt = db.prepare("INSERT INTO userinfo(user, pass, email) VALUES (?, ?, ?)");
-    const info = stmt.run(data.user, data.pass, data.email);
+    const stmt = db.prepare("INSERT INTO userinfo(email, pass) VALUES (?, ?)");
+    const info = stmt.run(data.pass, data.email);
     
     res.json({ "message": info.changes + " record created: ID " + info.lastInsertRowid + " (201)" });
     res.status(201);
 })
+
+
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {
@@ -70,13 +114,12 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", upload.none(), (req, res) => {
     
     var data = {
-        user: req.body.user,
         email: req.body.email,
         pass: req.body.pass ? md5(req.body.pass) : null
     }
 
     const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass), email = COALESCE(?, email) WHERE id = ?");
-    const info = stmt.run(data.user, data.pass, data.email, req.params.id);
+    const info = stmt.run( data.pass, data.email, req.params.id);
     
     res.json({ "message": info.changes + " record updated: ID " + req.params.id + " (200)" });
     res.status(200);
